5b9f4c17c6dc5046e467b6a3429dabd464862b8a
waka <p.c.spruijtenburg@utwente.nl>
2014-11-11 10:28:48 +0100

    Added new procedure to align writefield only on first device of a new chip

--- Multisample.js
+++ Multisample.js
@@ -869,50 +869,49 @@ function AlignWF(markprocedure, logWFflag, i, j, k)
 		// only do a writefield alignment on the very first device
 		case 4:
 				//new option: 4, for aligning writefield on photo, and then, ebl markers on the first device
-				if ((j == 0) && (k == 0)){
-				      App.Errmsg(EC_YESNO, 0 , "On first device?")
-									 	amf1 = createArray(3);
-										amf1[1] = AutoWFAlign(11);
-										Panicbutton();
-										if (amf1[1] > 0) 
-										{
-											amf1[2] = AutoWFAlign(12);
-											Panicbutton();
-										}
-										if (amf1[2] > 0) 
-										{
-											amf1[3] = AutoWFAlign(13)
-										}
-										if (logWFflag == 1)
-										{
-											logfile = App.OpenInifile(Glogfilename[1] + Glogfilename[2]);
-											logfile.WriteString("Failed markers S" + i,"Markprocedure", "Photo + EBL markers");
-											logstring = "Mark1=" + amf1[1] + ", Mark2 =" + amf1[2] + ", Mark3 =" + amf1[3]
-											logfile.WriteString("Failed markers S" + i,"D" + m + ";" + n + " (Photomarkers)", logstring);
-										}
-										Panicbutton();
-										
-										amf2 = createArray(3);
-										amf2[1] = AutoWFAlign(21);
-										Panicbutton();
-										if (amf2[1] > 0) 
-										{
-											amf2[2] = AutoWFAlign(22);
-											Panicbutton();
-										}
-										if (amf2[2] > 0) 
-										{
-											amf2[3] = AutoWFAlign(23)
-										}
-										if (logWFflag == 1)
-										{
-											logfile = App.OpenInifile(Glogfilename[1] + Glogfilename[2]);
-											logfile.WriteString("Failed markers S" + i,"Markprocedure", "Photo + EBL markers");
-											logstring = "Mark1=" + amf2[1] + ", Mark2 =" + amf2[2] + ", Mark3 =" + amf2[3]
-											logfile.WriteString("Failed markers S" + i,"D" + m + ";" + n + " (EBL markers)", logstring);
-										}
-										Panicbutton();
-										}
+				if ((j == 0) && (k == 0)) {
+					amf1 = createArray(3);
+					amf1[1] = AutoWFAlign(11);
+					Panicbutton();
+					if (amf1[1] > 0) 
+					{
+						amf1[2] = AutoWFAlign(12);
+						Panicbutton();
+					}
+					if (amf1[2] > 0) 
+					{
+						amf1[3] = AutoWFAlign(13)
+					}
+					if (logWFflag == 1)
+					{
+						logfile = App.OpenInifile(Glogfilename[1] + Glogfilename[2]);
+						logfile.WriteString("Failed markers S" + i,"Markprocedure", "Photo + EBL markers");
+						logstring = "Mark1=" + amf1[1] + ", Mark2 =" + amf1[2] + ", Mark3 =" + amf1[3]
+						logfile.WriteString("Failed markers S" + i,"D" + m + ";" + n + " (Photomarkers)", logstring);
+					}
+					Panicbutton();
+					
+					amf2 = createArray(3);
+					amf2[1] = AutoWFAlign(21);
+					Panicbutton();
+					if (amf2[1] > 0) 
+					{
+						amf2[2] = AutoWFAlign(22);
+						Panicbutton();
+					}
+					if (amf2[2] > 0) 
+					{
+						amf2[3] = AutoWFAlign(23)
+					}
+					if (logWFflag == 1)
+					{
+						logfile = App.OpenInifile(Glogfilename[1] + Glogfilename[2]);
+						logfile.WriteString("Failed markers S" + i,"Markprocedure", "Photo + EBL markers");
+						logstring = "Mark1=" + amf2[1] + ", Mark2 =" + amf2[2] + ", Mark3 =" + amf2[3]
+						logfile.WriteString("Failed markers S" + i,"D" + m + ";" + n + " (EBL markers)", logstring);
+					}
+					Panicbutton();
+					}
 					
 				break;
 		}
